278b7b399f5a670ef73a744a8e16351a5c3ad5d7
Takashi Kokubun <takashikkbn@gmail.com>
2017-10-22 16:57:50 +0900

    select limited columns in POST /login

--- apps/isubata/webapp/ruby/app.rb
+++ apps/isubata/webapp/ruby/app.rb
@@ -92,7 +92,7 @@ class App < Sinatra::Base
 
   post '/login' do
     name = params[:name]
-    statement = db.prepare('SELECT * FROM user WHERE name = ?')
+    statement = db.prepare('SELECT id, password, salt FROM user WHERE name = ? LIMIT 1')
     row = statement.execute(name).first
     if row.nil? || row['password'] != Digest::SHA1.hexdigest(row['salt'] + params[:password])
       return 403
