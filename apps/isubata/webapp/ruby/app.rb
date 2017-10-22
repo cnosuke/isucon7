40090b4642a0324f1d5ebefde81fac59cf317f38
Shinnosuke Takeda <shinnosuke@gmail.com>
2017-10-22 20:20:32 +0900

    add reseting auto_increment

--- apps/isubata/webapp/ruby/app.rb
+++ apps/isubata/webapp/ruby/app.rb
@@ -48,8 +48,11 @@ class App < Sinatra::Base
 
   get '/initialize' do
     db.query("DELETE FROM user WHERE id > 1000")
+    db.query("ALTER TABLE user AUTO_INCREMENT = 1001")
     db.query("DELETE FROM image WHERE id > 1001")
+    db.query("ALTER TABLE image AUTO_INCREMENT = 1002")
     db.query("DELETE FROM channel WHERE id > 10")
+    db.query("ALTER TABLE channel AUTO_INCREMENT = 11")
     db.query("DELETE FROM message WHERE id > 10000")
     db.query("ALTER TABLE message AUTO_INCREMENT = 10001")
     db.query("DELETE FROM haveread")
